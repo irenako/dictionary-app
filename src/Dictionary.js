65fa31309f4ba712bdf54c99c76eef6531c08c7f
irenako <108225581+irenako@users.noreply.github.com>
2022-09-11 18:11:05 +0300

    Added axios and search engine

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,8 +1,16 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+
+  axios
+    .get(`https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`)
+    .then(handleResponse);
+  function handleResponse(response) {
+    console.log(response[0]);
+  }
   function search(event) {
     alert(`Searching for ${keyword} definition`);
     event.preventDefault();
