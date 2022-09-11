ab63a7006003fb46856397b482a5ca1d71bf88a0
irenako <108225581+irenako@users.noreply.github.com>
2022-09-11 17:50:21 +0300

    Created a search engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <h1>Dictionary</h1>
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer>
+        It was coded by Iryna Koval and is{" "}
+        <a href="https" alt="link to GitHub">
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a href="https" alt="link to Netlify">
+          hosted on Netlify{" "}
+        </a>
+      </footer>
     </div>
   );
 }
